013532ca8dc6b4ffa5bf90098d66545100e45788
pratik <pratikshaha339@gmail.com>
2023-02-21 17:23:25 +0100

    Update main.groovy

--- cicd/jenkins/jenkins-main/main.groovy
+++ cicd/jenkins/jenkins-main/main.groovy
@@ -1,8 +1,17 @@
-node( ) {
-    stage('checkout') { 
-        checkout scm
-    }
-    def common = evaluate(readFile("cicd/jenkins/jenkins-common/common.groovy"))
-    print(common)
+# node( ) {
+#     def common = evaluate(readFile("cicd/jenkins/jenkins-common/common.groovy"))
+#     print(common)
+#     common.deploy("test")
+# }
+
+
+def checkoutGitDataManagement(branch) {    
+    checkout([$class: 'GitSCM', branches: [[name: "*/$branch"]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'GitLFSPull'], [$class: 'CloneOption', depth: 0]], submoduleCfg: []])    
+}
+
+node( ) {    
+    checkoutGitDataManagement(env.BRANCH_NAME)    
+    def common = evaluate readFile('cicd/jenkins/jenkins-common/Common.groovy')    
+    
     common.deploy("test")
 }
