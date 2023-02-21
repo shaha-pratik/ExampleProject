dd603facc2a2f66ddfa1b94bd4796e25e30e30ed
pratik <pratikshaha339@gmail.com>
2023-02-21 18:25:38 +0100

    Update main.groovy

--- cicd/jenkins/jenkins-main/main.groovy
+++ cicd/jenkins/jenkins-main/main.groovy
@@ -1,12 +1,19 @@
 def checkoutGitDataManagement(branch) {    
     checkout([$class: 'GitSCM', branches: [[name: "*/$branch"]], extensions: [[$class: 'CloneOption', depth: 0]]])    
 }
-
-node( ) {
+def origin() {
     echo "@@@@@@ I am new here"
     echo env.BRANCH_NAME
     echo "@@@@@@Before Checkout"
     checkoutGitDataManagement(env.BRANCH_NAME)
     echo "@@@@@@After Checkout"
+}
+node( ) {
+    stage ('Checkout') {
+        checkout scm
+    }
+    stage ('First test') {
+        sh 'mvn help:evaluate -Dexpression=project.version -q -DforceStdout'
+    }
 
 }
