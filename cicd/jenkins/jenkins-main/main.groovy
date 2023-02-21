7cafbcab529248256d289b46500bd66b9b1e2389
pratik <pratikshaha339@gmail.com>
2023-02-21 16:48:29 +0100

    Update main.groovy

--- cicd/jenkins/jenkins-main/main.groovy
+++ cicd/jenkins/jenkins-main/main.groovy
@@ -2,6 +2,6 @@ node( ) {
     stage('checkout') { 
         checkout scm
     }
-    common = evaluate readFile("cicd/jenkins/jenkins-common/common.groovy")
+    def common = evaluate readFile("cicd/jenkins/jenkins-common/common.groovy")
     common.deploy("test")
 }
